1e3995bdaba7b0b5c0dde981e5adfebe9e55c7ec
temurtas <halil.temurtas@metu.edu.tr>
2018-07-15 15:59:33 +0300

    exit key in place

--- project1.py
+++ project1.py
@@ -23,8 +23,8 @@ while True:
             cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0, 255, 0),2)
 
     cv2.imshow('img', img)
-    k= cv2.waitKey(30)  & 0xff
-    if k==27:
+
+    if cv2.waitKey(30) & 0xff == ord('q'):
         break
 
 cap.release()
