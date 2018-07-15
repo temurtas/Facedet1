f57e1d411b6fb885e7216e85e47d4f3de269359e
temurtas <halil.temurtas@metu.edu.tr>
2018-07-15 15:47:00 +0300

    working code on yoga 710

--- project1.py
+++ project1.py
@@ -10,7 +10,6 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
@@ -20,8 +19,8 @@ while True:
 
         eyes = eye_cascade.detectMultiScale(roi_gray)
 
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0, 255, 0),2)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0, 255, 0),2)
 
     cv2.imshow('img', img)
     k= cv2.waitKey(30)  & 0xff
